15b08e549d70377104a42fbfcd2e325539c1f990
zanehala <zmhala@gmail.com>
2022-09-21 21:59:00 -0500

    meh

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from whitenoise import WhiteNoise
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="/public", index_file=True)
 metrics = GunicornInternalPrometheusMetrics(app)
+
+print(request.remote_addr)
 
 @app.route("/test")
 def main():
